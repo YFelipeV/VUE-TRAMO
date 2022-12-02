b8b3af8bf541a29d2f0e0f7ed575a70370bcd7fb
felipe <yfsegura04@misena.edu.co>
2022-12-02 10:27:52 -0500

    acomode ruta principal y los vinculos en el nav

--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -1,6 +1,6 @@
 <template>
 <nav class="navbar navbar-expand-lg navbar-primary fixed-top   bg-light" data-navbar-on-scroll="data-navbar-on-scroll">
-    <div class="container"><a class="navbar-brand d-flex align-items-center fw-bold fs-2  " href="index.html">
+    <div class="container"><a class="navbar-brand d-flex align-items-center fw-bold fs-2  " href="/">
             <div class="text-warning">TRAMO</div>
             <div class="text-1000">S.A.S</div>
         </a>
@@ -9,14 +9,14 @@
             aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse border-top border-lg-0 mt-1 mt-lg-0" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto pt-2 pt-lg-0 ">
-                <li class="nav-item"><a class="nav-link fw-medium  active text-primary" aria-current="page" href="#home">Inicio</a>
+                <li class="nav-item"><a class="nav-link fw-medium  active text-primary" aria-current="page" href="/">Inicio</a>
                 </li>
-                <li class="nav-item"><a class="nav-link fw-medium active text-primary" aria-current="page" href="./about.html">Acerca
+                <li class="nav-item"><a class="nav-link fw-medium active text-primary" aria-current="page" href="#/about">Acerca
                         de
                         nosotros</a></li>
                 <li class="nav-item"><a class="nav-link fw-medium text-primary" href="#pricing">Testimonios</a>
                 </li>
-                <li class="nav-item"><a class="nav-link fw-medium text-primary" href="./contactos.html">Contactos</a>
+                <li class="nav-item"><a class="nav-link fw-medium text-primary" href="#/contactos">Contactos</a>
                 </li>
 
             </ul>
